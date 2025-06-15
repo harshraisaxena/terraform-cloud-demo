c515f14b592fafbb6424b9b6e938f3a84b3bf55d
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2025-06-15 22:10:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,17 +19,12 @@ filter {
 resource "aws_instance" "myec2" {
   ami    = data.aws_ami.myami.id 
   instance_type = "t2.micro"
-tags = {
-    Name = "dev-env-01"
-  }
- 
+
 }
 
 resource "aws_instance" "myec2-2" {
   ami    = data.aws_ami.myami.id 
   instance_type = "t2.micro"
-tags = {
-    Name = "dev-env-02"
-  }
+
  
 }
