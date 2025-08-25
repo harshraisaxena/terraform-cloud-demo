fcc8eebcfb47662b1728ecdf05a95936df011f46
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2025-08-25 08:59:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,28 @@ region = "us-east-1"
 
 }
 
+data "aws_ami" "myami"{
+
+owners = ["amazon"]
+most_recent = true
+
+filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+
+}
+
+resource "aws_instance" "myec2" {
+  ami    = data.aws_ami.myami.id 
+  instance_type = "t2.micro"
+
+}
+
+resource "aws_instance" "myec2-2" {
+  ami    = data.aws_ami.myami.id 
+  instance_type = "t2.medium"
+
+ 
+}
